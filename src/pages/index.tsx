fe89569ce6ede8d73aa97358c4fafec299b2793b
BePoyerGabe <bernardopoyer00@gmail.com>
2021-04-13 22:04:15 -0300

    Header Component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,17 @@
-import styles from '../styles/home.module.scss'
+
+import Head from 'next/head'
 
 export default function Home() {
-  
+  return (
+    <>
+      <Head>
+        <title>
+          Inicio | IgNews
+        </title>
+      </Head>
+      <h1>  
+        Hello WORLD
+      </h1>
+    </>
+  )
 }
